2f8b1cd66bc3286d8694c00699393207c0f4e92b
Mehdi Hattak <vics06@hotmail.fr>
2019-06-26 13:45:10 +0200

    Configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { GameListComponent } from './pages/game-list/game-list.component';
+import { GameInfoComponent } from './pages/game-info/game-info.component';
+import { GameCreateComponent } from './pages/game-create/game-create.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GameListComponent },
+  { path: 'view/:id', component: GameInfoComponent},
+  { path: 'create', component: GameCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
