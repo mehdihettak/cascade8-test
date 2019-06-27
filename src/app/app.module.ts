e77b19135a06c457fd839da034d0d39c87a11894
mehdi hettak <vics06@hotmail.fr>
2019-06-27 23:20:40 +0200

    create cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule, MatCardModule,
   MatFormFieldModule, MatCheckboxModule, MatDatepickerModule,
   MatRadioModule, MatSelectModule, MatInputModule, MatNativeDateModule} from '@angular/material';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { MatButtonModule, MatCardModule,
     AppComponent,
     GameListComponent,
     GameInfoComponent,
-    GameCreateComponent
+    GameCreateComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
